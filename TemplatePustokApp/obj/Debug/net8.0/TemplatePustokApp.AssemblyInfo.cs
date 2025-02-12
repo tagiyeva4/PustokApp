11832e581583cf9ddb824c80d667373a7a77eabc
tagiyeva4 <aysuht@code.edu.az>
2025-02-12 13:50:30 +0400

    features added

--- TemplatePustokApp/obj/Debug/net8.0/TemplatePustokApp.AssemblyInfo.cs
+++ TemplatePustokApp/obj/Debug/net8.0/TemplatePustokApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TemplatePustokApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c329ab3456f24fcdf679eb5a5db59d2ebdcc8407")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f97511c74c1e48f715f6ee5febe292f8ee5da0f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TemplatePustokApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TemplatePustokApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
